9d7ff8ac0819a11c0f9bac548379c5708f9b2fa4
Diogo Nascimento <64949287+diogobraganascimento@users.noreply.github.com>
2024-06-03 17:05:50 -0300

    Testando repositório de listagem de usuários

--- src/model/repository/create_user_repository_test.go
+++ src/model/repository/create_user_repository_test.go
@@ -12,21 +12,26 @@ import (
 )
 
 func TestUserRepository(t *testing.T) {
-	database_name := "user_database_test"
-	collection_name := "user_collection_test"
+	databaseName := "user_database_test"
+	collectionName := "user_collection_test"
 
-	os.Setenv("MONGODB_USER_DB", collection_name)
+	err := os.Setenv("MONGODB_USER_DB", collectionName)
+	if err != nil {
+		t.FailNow()
+		return
+	}
 	defer os.Clearenv()
 
 	mtestDb := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
 
+	// Testing creating user success
 	mtestDb.Run("when_sending_a_valid_domain_reurns_success", func(mt *mtest.T) {
 		mt.AddMockResponses(bson.D{
 			{Key: "ok", Value: 1},
 			{Key: "n", Value: 1},
 			{Key: "acknowledged", Value: true},
 		})
-		databaseMock := mt.Client.Database(database_name)
+		databaseMock := mt.Client.Database(databaseName)
 
 		repo := NewUserRepository(databaseMock)
 		domain := model.NewUserDomain(
@@ -44,11 +49,12 @@ func TestUserRepository(t *testing.T) {
 		assert.EqualValues(t, userDomain.GetPassword(), domain.GetPassword())
 	})
 
+	// Testing creating user error
 	mtestDb.Run("return_error_from_database", func(mt *mtest.T) {
 		mt.AddMockResponses(bson.D{
 			{Key: "ok", Value: 0},
 		})
-		databaseMock := mt.Client.Database(database_name)
+		databaseMock := mt.Client.Database(databaseName)
 
 		repo := NewUserRepository(databaseMock)
 		domain := model.NewUserDomain(
